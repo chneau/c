2dec276a6f73900c57f3327f4daf394650807190
chneau <charles63500@gmail.com>
2024-11-08 19:17:51 +0000

    Refactor build system and update run_command function

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,21 @@
 const std = @import("std");
 
+const allocator = std.heap.page_allocator;
+
 pub fn split_str(str: []const u8) []const []const u8 {
     var parts = std.mem.split(u8, str, " ");
-    var result = std.ArrayList([]const u8).init(std.heap.page_allocator);
+    var result = std.ArrayList([]const u8).init(allocator);
     while (parts.next()) |part| result.append(part) catch return result.items;
     return result.items;
 }
 
-pub fn run_command(argv: []const u8) !void {
-    const x = split_str(argv);
-    var child = std.process.Child.init(x, std.heap.page_allocator);
+pub fn run_command(cmd: []const u8) !void {
+    const argv = split_str(cmd);
+    var child = std.process.Child.init(argv, allocator);
     try child.spawn();
     _ = try child.wait();
 }
 
 pub fn main() !void {
-    std.debug.print("Hello, world!\n", .{});
     try run_command("echo Hello, world!");
 }
